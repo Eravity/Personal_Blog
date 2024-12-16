20cc633a84014791d2acee8cf9014c653d1f5cb2
Eravity <cebotaricristian92@gmail.com>
2024-12-16 23:38:11 +0200

    Add fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,41 @@
-import "./globals.css";
+import { Montserrat, PT_Serif } from "next/font/google";
+import { Metadata } from "next";
 import { ReactNode, JSX } from "react";
 
-interface layoutProps {
+type LayoutProps = {
   children: ReactNode;
-}
+};
 
-export const metadata = {
-  title: 'Personal Blog',
+export const ptSerif = PT_Serif({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: "swap",
+});
+
+export const metadata: Metadata = {
+  title: "Personal Blog",
   openGraph: {
-    title: 'Personal Blog First Page',
-    description: 'This website is a personal blog website.',
+    title: "Personal Blog First Page",
+    description: "This website is a personal blog website.",
   },
-}
+};
 
-const layoutProps: React.FC<layoutProps> = ({ children }): JSX.Element => {
+const Layout: React.FC<LayoutProps> = ({ children }): JSX.Element => {
   return (
     <html>
-      <head>
-      </head>
-      <body>{children}</body>
+      <head></head>
+      <body>
+        <header></header>
+        <main>{children}</main>
+        <footer></footer>
+      </body>
     </html>
   );
 };
 
-export default layoutProps;
+export default Layout;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import React, { JSX } from 'react';
+import React, { JSX } from "react";
 
 const page: React.FC = (): JSX.Element => {
   return (
     <div>
-      <h1></h1>
+      <h1 className="max-w-md font-normal font-sans">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est veniam earum magnam harum ullam ex facilis repellat necessitatibus, hic fugit natus unde mollitia velit voluptatem iste dolor? Natus, tempore sequi.
+      </h1>
+      <hr />
+      <h1 className="max-w-md font-serif">
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est aut provident animi deleniti omnis, facere a! Odit voluptas, aut soluta facilis commodi quam nulla. Impedit itaque incidunt nostrum possimus debitis.
+      </h1>
     </div>
   );
 };
